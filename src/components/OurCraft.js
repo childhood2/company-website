4c242e5d88da6f700a8b82f83a6dc3e6580f8099
childhood2 <chinookaponi@gmail.com>
2026-02-07 12:49:02 -0800

    our craft block separates

--- src/components/OurCraft.js
+++ src/components/OurCraft.js
@@ -37,37 +37,42 @@ function OurCraft() {
 
       <div className="app__spacer"></div>
 
-      <div className="app__jumbotronSmall">
-        <div className="about__desc">
-          <h3>The BarterPH - the true essence of free trading</h3>
-          <p>
-            In just two months, we were able to developed and launched the
-            BarterPh App mobile application in our goal to help the farmers
-            trade their fresh goods in exchange for non-commercial items they
-            need in their household without spending a peso. Wait, there's more!
-            The BarterPh App can be used by anyone too and enjoy the features of
-            the platform for efficient and free trading. Try it and let us know
-            how we can improve it so you can forever enjoy it for free!
-          </p>
-          <br></br>
-          <h3>PlanetReviews - revolutionizing consumer reviews</h3>
-          <p>
-            The PlanetReviews is still under development phase as this platform
-            showcases an ecosystem where consumers are aimed to be democratized
-            from ages of being misled by dishonest reviews where they mostly end
-            up wasting their hard-earned money in mediocre products and
-            services. Get thrilled and be part of an awesome platform where you
-            can find the biggest community of honest and reliable reviews and
-            get rewarded. Want to have a sneak peak? Checkout&nbsp;
-            <a
-              href="https://www.theplanetreviews.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="ourCraft__tpr">theplanetreviews.com</span>
-            </a>
-            for more information.
-          </p>
+      <div className="app__jumbotronSmall ourCraft__jumbotronPanels">
+        <div className="ourCraft__descItem">
+          <div className="about__desc">
+            <h3>The BarterPH - the true essence of free trading</h3>
+            <p>
+              In just two months, we were able to developed and launched the
+              BarterPh App mobile application in our goal to help the farmers
+              trade their fresh goods in exchange for non-commercial items they
+              need in their household without spending a peso. Wait, there's more!
+              The BarterPh App can be used by anyone too and enjoy the features of
+              the platform for efficient and free trading. Try it and let us know
+              how we can improve it so you can forever enjoy it for free!
+            </p>
+          </div>
+        </div>
+        <div className="ourCraft__descItem">
+          <div className="about__desc">
+            <h3>PlanetReviews - revolutionizing consumer reviews</h3>
+            <p>
+              The PlanetReviews is still under development phase as this platform
+              showcases an ecosystem where consumers are aimed to be democratized
+              from ages of being misled by dishonest reviews where they mostly end
+              up wasting their hard-earned money in mediocre products and
+              services. Get thrilled and be part of an awesome platform where you
+              can find the biggest community of honest and reliable reviews and
+              get rewarded. Want to have a sneak peak? Checkout&nbsp;
+              <a
+                href="https://www.theplanetreviews.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="ourCraft__tpr">theplanetreviews.com</span>
+              </a>
+              for more information.
+            </p>
+          </div>
         </div>
       </div>
 
